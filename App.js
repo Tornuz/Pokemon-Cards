829d8568d77b148bd1af334e1cf23716098df50e
Tornuz <yadavgourav2001@gmail.com>
2024-02-15 17:59:33 +0530

    Second-commit

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Platform } from 'react-native';
+import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+
+  const charmanderData = {
+    name: "Charmander",
+    image: require("./assets/charmander.png"),
+    type: "Fire",
+    hp: 39,
+    moves: ["Scratch", "Ember", "Growl", "Leer"],
+    weaknesses: ["Water", "Rock"],
+  };
+
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), 
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"),
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"),
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='auto' />
+      <PokemonCard  {...charmanderData} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: Platform.OS === 'android' ? 35 : 0
   },
 });
---
+++ components/PokemonCard.js
@@ -0,0 +1,106 @@
+import { View, Text, StyleSheet, Platform, Image } from 'react-native'
+import React from 'react'
+
+const getTypeDetails = (type) => {
+  switch (type.toLowerCase()) {
+    case "electric":
+      return { borderColor: "#FFD700", emoji: "⚡️" };
+    case "water":
+      return { borderColor: "#6493EA", emoji: "💧" };
+    case "fire":
+      return { borderColor: "#FF5733", emoji: "🔥" };
+    case "grass":
+      return { borderColor: "#66CC66", emoji: "🌿" };
+    default:
+      return { borderColor: "#A0A0A0", emoji: "❓" };
+  }
+};
+
+export default function PokemonCard({name, image, type, hp, moves, weaknesses}) {
+  
+  const {borderColor, emoji} = getTypeDetails(type)
+
+  return (
+    <View style={styles.card}>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.hp}>❤️{hp}</Text>
+      </View>
+      <Image style={styles.image} resizeMode='contain' source={image} accessibilityLabel={`${name} Pokemon`} />
+
+      <View style={styles.typeContainer}>
+        <View style={[styles.badge, { borderColor }]}>
+          <Text style={styles.typeEmoji}>{emoji}</Text>
+          <Text style={styles.typeText}>{type}</Text>
+        </View>
+      </View>
+
+      <View>
+        <Text>Moves: {moves.join(", ")}</Text>
+      </View>
+
+      <View>
+        <Text>Weakness: {weaknesses.join(", ")}</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 16,
+    borderWidth: 2,
+    padding: 16,
+    margin: 16,
+
+    ...Platform.select({
+      ios: {
+        shadowOffset: { width: 2, height: 2},
+        shadowColor: "#333",
+        shadowOpacity: 0.3,
+        shadowRadius: 4
+      },
+      android: {
+        elevation: 5
+      }
+    })
+  },
+  image: {
+    height: 200,
+    width: "100%"
+  },
+  nameContainer: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    marginBottom: 32
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: "bold"
+  },
+  hp: {
+    fontSize: 22
+  },
+  typeContainer: {
+    alignItems: "center",
+    marginBottom: 40,
+    marginTop: 20
+  },
+  badge: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 20,
+    borderWidth: 4
+  },
+  typeEmoji: {
+    fontSize: 30,
+    marginRight: 12
+  },
+  typeText: {
+    fontSize: 22,
+    fontWeight: "bold"
+  }
+})
